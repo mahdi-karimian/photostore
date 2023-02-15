02ff4576195dcd0315e3affe7b1e8998adeff798
Mahdi <pckarimian@gmail.com>
2023-02-15 14:39:31 +0330

    create categories implemented

--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -30,4 +30,11 @@ class CategoriesController extends Controller
             return back()->with('success', 'دسته بندی با موفقیت اینجاد شد ');
         }
     }
+
+    public function all()
+    {
+        $categories = Category::all();
+
+        return view('admin.categories.all',compact('categories'));
+    }
 }
