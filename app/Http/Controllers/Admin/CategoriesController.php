8a403ef4570118c4514107d1d5c252da20e7c5c5
Mahdi <pckarimian@gmail.com>
2023-02-15 12:10:15 +0330

    storing categories emplemented

---
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\StoreRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+
+    public function store(StoreRequest $request)
+    {
+        $validatedDate = $request->validated();
+        $createdCategory = Category::create([
+            'title' => $validatedDate['title'],
+            'slug' => $validatedDate['slug'],
+        ]);
+
+        if (!$createdCategory) {
+
+            return back()->with('failed', 'دسته بندی ایجاد نشد ');
+        } else {
+            return back()->with('success', 'دسته بندی با موفقیت اینجاد شد ');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,15 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoriesController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-Route::get('products/all', function () {
-    return view('frontend.products.all');
-});
-Route::get('admin', function () {
-    return view('admin.index');
-});
-Route::get('admin/users', function () {
-    return view('admin.users.index');
+//admin/categories/create
+
+Route::prefix('admin')->group(function () {
+    Route::prefix('categories')->group(function () {
+        Route::get('create', [CategoriesController::class, 'create']);
+        Route::post('', [CategoriesController::class, 'store'])->name('admin,categories.store');
+    });
 });
 
 
